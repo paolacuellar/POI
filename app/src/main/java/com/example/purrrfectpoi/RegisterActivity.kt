bb95b7a459fd74172f3440e0a4c92bcc05c07ee5
Luis Angel Daniel Sanchez <luis_dani.s@hotmail.com>
2022-03-06 19:04:16 -0600

    Paginas de Login y Registro

---
+++ app/src/main/java/com/example/purrrfectpoi/RegisterActivity.kt
@@ -0,0 +1,23 @@
+package com.example.purrrfectpoi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.example.purrrfectpoi.fragments.ChatsFragment
+import com.example.purrrfectpoi.fragments.GruposFragment
+import com.example.purrrfectpoi.fragments.MuroFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class RegisterActivity : AppCompatActivity() {
+
+    val TAG="RegisterActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+}
+
+
