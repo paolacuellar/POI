a7596faccc97509e6a83f097c39a5919087fba35
pao_cuellar <pao.cuellar18@hotmail.com>
2022-03-14 18:33:07 -0600

    Pantalla de Editar/Crear Grupo, pantalla de "Nueva Publicación" y correcciones

---
+++ app/src/main/java/com/example/purrrfectpoi/Group_AC_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.purrrfectpoi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Group_AC_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_ac)
+    }
+}
